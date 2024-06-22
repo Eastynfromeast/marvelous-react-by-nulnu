f944c1d403c6caedf5c609822a63d5aa28eb9888
Eastyn <eastynwork@gmail.com>
2024-06-22 16:44:04 +0900

    first commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+function Detail() {
+	return (
+		<div>
+			<h1>Marvelous React Detail</h1>
+		</div>
+	);
+}
+
+export default Detail;
---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+	return (
+		<div>
+			<h1>Marvelous React Home</h1>
+		</div>
+	);
+}
+
+export default Home;
