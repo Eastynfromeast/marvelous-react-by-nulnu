e24e746f1c5f92fbd0424c4ae40ff6c317f50e8c
Eastyn <eastynwork@gmail.com>
2024-06-22 17:47:37 +0900

    set basic routes of pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+	const { id } = useParams();
+	const heroDataUrl = `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}`;
+	const [heroData, setHeroData] = useState("");
+
+	useEffect(() => {
+		const getDetails = async () => {
+			const json = await (await fetch(heroDataUrl)).json();
+			setHeroData(json.data.results[0]);
+		};
+		getDetails();
+	}, [id, heroDataUrl]);
+	console.log(heroData);
 	return (
 		<div>
 			<h1>Marvelous React Detail</h1>
+			{heroData !== "" && (
+				<>
+					<h2>{heroData.name}</h2>
+				</>
+			)}
 		</div>
 	);
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 function Home() {
+	const [heroes, setHeroes] = useState("");
+	const MARVEL_URL = "https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023";
+	const getMovies = async () => {
+		const json = await (await fetch(MARVEL_URL)).json();
+		setHeroes(json.data.results);
+	};
+	console.log(heroes);
+	const createImageURL = data => {
+		// if(data.path.indexOf("image_not_available"))
+		return data.path + "." + data.extension;
+	};
+	useEffect(() => {
+		getMovies();
+	}, [MARVEL_URL]);
 	return (
 		<div>
 			<h1>Marvelous React Home</h1>
+			{heroes !== "" && (
+				<>
+					<ul>
+						{heroes.map(hero => {
+							return (
+								<li key={hero.id}>
+									<Link to={`/detail/${hero.id}`}>
+										<img src={createImageURL(hero.thumbnail)} alt={hero.name} />
+										<h3>{hero.name}</h3>
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</>
+			)}
 		</div>
 	);
 }
